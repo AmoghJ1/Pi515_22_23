ddd141cb431e407145a4281ee5a86e65ac7276a7
AmoghJ1 <>
2022-11-10 22:35:26 +0000

    Finished project 1

--- Project1/main.py
+++ Project1/main.py
@@ -3,27 +3,32 @@
 import random
 
 keepAsking = True
+
 while keepAsking:
     num = input('Type a number for an upper bound: ')
     if num.isdigit():
         print("Let's play!")
         num = int(num)
+        break
         # something about the loop condition
     else:
         print("Invalid input. Try again.")
+      
 
 secret = random.randint(1, num)
 guess = None
-count = 1
+count = 0
 
 while guess != secret:
     guess = input("Type a number between 1 and " + str(num) + ": ")
     if guess.isdigit():
         guess = int(guess)
-    if False: # can't always be wrong
+    if guess == secret: # can't always be wrong  
+        count += 1
         print("You got it!")
     else:
         print("Try again.")
+        count += 1
         # what else?
 
 if count == 1:
